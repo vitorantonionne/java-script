62afef00be4945ebb57241d421a9360b89b0cd8b
Vitor Antonionne <114239671+vitorantonionne@users.noreply.github.com>
2022-11-22 12:14:04 -0300

    condições simples e composta if/else

---
+++ ex007-condições/main01.js
@@ -0,0 +1,14 @@
+var pais = 'Brasil'
+if (pais == 'Brasil') {
+    console.log ('Brasileiro')
+}else {
+    console.log('Estrangeiro')
+}
+
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais != 'Brasil') {
+    console.log ('Você é Estrangeiro')
+}else {
+    console.log('Você é Brasileiro')
+}

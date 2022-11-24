5949a0888b60db917e06b354247e56c4f0f6aa68
Vitor Antonionne <114239671+vitorantonionne@users.noreply.github.com>
2022-11-24 11:56:53 -0300

    pequeno desafio condição composta

--- ex010-praticas/main.js
+++ ex010-praticas/main.js
@@ -1,7 +1,27 @@
 function carregar() {
     var msg = document.querySelector('p#msgg')
-    var img = document.querySelector('div#imgg')
+    var imge = document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são  ${hora} horas`
+    
+    if (hora >=0 && hora < 6) {
+        msg.innerHTML = `Agora são ${hora} hora`
+        imge.src = 'imagens/madru.img.jpg'
+        document.body.style.backgroundColor = '#515159'
+    }else if (hora < 12) {
+        msg.innerHTML = `Agora são ${hora} hora`
+        imge.src = 'imagens/dia.img.jpg'
+        document.body.style.backgroundColor = 'rgb(252, 252, 166)'
+
+    }else if (hora <= 18 ) {
+        msg.innerHTML = `Agora são ${hora} hora`
+        imge.src = 'imagens/tarde.img.jpg'
+        document.body.style.backgroundColor = '#A8B0BF'
+
+    }else if (hora > 18){
+        msg.innerHTML = `Agora são ${hora} hora`
+        imge.src = 'imagens/noite.img.jpg'
+        document.body.style.background = '#262626'
+
+    }
 }

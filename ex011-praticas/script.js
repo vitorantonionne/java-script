7b6050a75190aaab7289d3c5e4d4af2b94ea5631
Vitor Antonionne <114239671+vitorantonionne@users.noreply.github.com>
2022-11-27 09:47:08 -0300

    att

---
+++ ex011-praticas/script.js
@@ -0,0 +1,8 @@
+function verificar() {
+    var nasc = Number(document.querySelector('input#inasc').value)
+    var masc = document.querySelector('input#isexm')
+    var fem = document.querySelector('input#isexm')
+    var idad = 2022 - nasc
+    
+    
+}

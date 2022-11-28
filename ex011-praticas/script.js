4d0aefd1fc3b5e6cc34048bb34d4c0f41943b756
Vitor Antonionne <114239671+vitorantonionne@users.noreply.github.com>
2022-11-28 19:40:33 -0300

    js

--- ex011-praticas/script.js
+++ ex011-praticas/script.js
@@ -1,8 +1,24 @@
 function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
     var nasc = Number(document.querySelector('input#itxtano').value)
-    var masc = document.querySelector('input#isexm')
-    var fem = document.querySelector('input#isexm')
+    var res = document.getElementById('resp')
     var idad = 2022 - nasc
+    
+    if (nasc == 0 || nasc > ano) {
+        alert(`ERROR Verifique as infomações novamente`)
+    } else {
+        var fsex = document.getElementsByName('sexo')
+        var genero = ''
 
+        if (fsex[0].checked) {
+            genero = 'Homem'
+        }else if (fsex[1].checked) {
+            genero = 'Mulher'
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = (`Sou ${genero} e tenho ${idad} de idade`)  
+        
+    }
     
 }

c7e9767c414399f50a45a5c2b23dd23d20090a68
Vitor Antonionne <114239671+vitorantonionne@users.noreply.github.com>
2022-12-01 12:12:59 -0300

    updates js

--- ex011-praticas/script.js
+++ ex011-praticas/script.js
@@ -9,15 +9,36 @@ function verificar() {
         alert(`ERROR Verifique as infomações novamente`)
     } else {
         var fsex = document.getElementsByName('sexo')
-        var genero = ''
+        var genero = '' 
+        var img = document.createElement('img')
+        img.setAttribute('id', 'Foto')
 
         if (fsex[0].checked) {
             genero = 'Homem'
+            if (idad > 0 && idad <10) {
+                img.setAttribute('src', 'imagens/foto-bebe-m.png')
+            }else if (idad < 21) {
+                img.setAttribute('src', 'imagens/foto-jovem-m.png')
+            }else if (idad < 50) {
+                img.setAttribute('src', 'imagens/foto-adulto-m.png')
+            }else {
+                img.setAttribute('src', 'imagens/foto-idoso-m.png')
+            }
         }else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idad > 0 && idad <10) {
+                img.setAttribute('src', 'imagens/foto-bebe-f.png')
+            }else if (idad < 21) {
+                img.setAttribute('src', 'imagens/foto-jovem-f.png')
+            }else if (idad < 50) {
+                img.setAttribute('src', 'imagens/foto-adulto-f.png')
+            }else {
+                img.setAttribute('src', 'imagens/foto-idoso-f.png')
+            }
         }
         res.style.textAlign = 'center'
-        res.innerHTML = (`Sou ${genero} e tenho ${idad} de idade`)  
+        res.innerHTML = (`Sou ${genero} e tenho ${idad} de idade`)
+        res.appendChild(img)
         
     }
     

3aee74e85f04bdbe6df35b69a403a12461a41aa1
Vitor Antonionne <114239671+vitorantonionne@users.noreply.github.com>
2022-12-02 12:31:02 -0300

    repetições do/while

---
+++ ex012-repetições/ambiente.js
@@ -0,0 +1,24 @@
+// console.log ('Ola tudo bem')
+// console.log ('Ola tudo bem')
+// console.log ('Ola tudo bem')
+// console.log ('Ola tudo bem')
+// console.log ('Ola tudo bem')
+// console.log ('Ola tudo bem')
+
+
+var c = 1
+while (c <= 6) {
+    console.log ('Ola tudo bem')
+    c++ 
+}
+
+
+var c = 1 
+
+do {
+    console.log (`Passo ${c}`)
+    c++
+}while(c <= 6)
+
+
+

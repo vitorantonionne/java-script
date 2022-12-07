835e197689259ef9dbc46bbdebe101d4bc8cb6cd
Vitor Antonionne <114239671+vitorantonionne@users.noreply.github.com>
2022-12-07 18:47:28 -0300

    Desafio completo

--- ex014-desafios/script.js
+++ ex014-desafios/script.js
@@ -1,21 +1,18 @@
 function tabuada() {
     let num = document.getElementById('itxtnum')
-    let tabuada = document.getElementsByName('itabuada')
+    let tabuada = document.getElementById('itabuada')
 
     if (num.value.length == 0) {
         alert ('Por favor, digite um numero!')
     }else {
         let x = Number(num.value)
-        let c = 0
-        // while (c <= 10 ){
-        //     c++
-            
-        // }
-        for (let c = 0 ; c <= 10 ; c++) {
-
-            let res = x*c
-            tabuada[0].innerHTML = `${x} x ${c} = ${res}`
-            
+        tabuada.innerHTML = ''
+            for (let c = 0 ; c <= 10 ; c++) {
+                let item = document.createElement('option')
+                item.value = `tabuada${c}`
+                let res = x*c
+                 item.text = `${x} x ${c} = ${res}`
+                tabuada.appendChild(item)      
         }
     }
 }

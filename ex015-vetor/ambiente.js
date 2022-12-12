4cad2102e43918d9c9dc7ea4375a7ccb4fed3b5b
Vitor Antonionne <114239671+vitorantonionne@users.noreply.github.com>
2022-12-12 19:04:00 -0300

    Update ambiente.js

--- ex015-vetor/ambiente.js
+++ ex015-vetor/ambiente.js
@@ -6,3 +6,10 @@ console.log(num)
 console.log (num.sort(function(a,b){return a-b}))
 console.log (`Valor do nossos vetores ${num.length}`)
 console.log (`Qual o primeiro valor do vetor ${num[0]}`)
+
+let pos = num.indexOf(10)
+if (pos == -1) {
+    console.log ('NUMERO INEXISTENTE')
+}else{
+    console.log (`O valor está na posição ${pos}`)
+}

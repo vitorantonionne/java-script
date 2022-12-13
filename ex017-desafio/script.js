e1339611a6f340aee67ec81c97bd8ea45660fa44
Vitor Antonionne <114239671+vitorantonionne@users.noreply.github.com>
2022-12-13 19:48:35 -0300

    add

---
+++ ex017-desafio/script.js
@@ -0,0 +1,10 @@
+function add(n){
+    let num = document.getElementById('itxtnum')
+    if (num.value.length == 0) {
+        alert ('Valor invalido ou ja encotnrado na lista')
+    }else {
+        let n = Number(num.value)
+
+    }
+ 
+}

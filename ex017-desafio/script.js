b489f4b60bf821032eec3afd3ef8d8469e8986c0
Vitor Antonionne <114239671+vitorantonionne@users.noreply.github.com>
2022-12-14 12:20:06 -0300

    reset

--- ex017-desafio/script.js
+++ ex017-desafio/script.js
@@ -1,10 +1,8 @@
-function add(n){
-    let num = document.getElementById('itxtnum')
-    if (num.value.length == 0) {
-        alert ('Valor invalido ou ja encotnrado na lista')
-    }else {
-        let n = Number(num.value)
+let num = document.getElementById('itxtnum')
+let lista = document.getElementById('ilista')
+let res = document.querySelector('div#ress')
+let valores = []
 
-    }
- 
+if () {
+
 }
